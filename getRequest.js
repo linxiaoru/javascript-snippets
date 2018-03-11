d484eb16c83f44b3e488a80803ae0ab65f6b12c8
linxiaoruxr <linxiaoruxr@163.com>
2018-03-11 23:51:29 +0800

    Update the comments

--- getRequest.js
+++ getRequest.js
@@ -1,12 +1,12 @@
 /**
- * 获取 url ？后面的所有参数，根据需要获取参数值
+ * get url parameters
 */
 function GetRequest() {
-  var url = location.search; // 获取url中"?"符后的字串
+  var url = location.search;  // get query string from url
   var theRequest = new Object();
   if (url.indexOf("?") != -1) {
     var str = url.substr(1);
-    strs = str.split("&");
+    strs = str.split("&");  // split our query string into its component parts
     for (var i = 0; i < strs.length; i++) {
       theRequest[strs[i].split("=")[0]] = (strs[i].split("=")[1]);
     }
@@ -14,7 +14,7 @@ function GetRequest() {
   return theRequest;
 }
 
-// 使用方法
+// useage
 var Request = new Object();
 Request = GetRequest();
 var a, b, c, d;
